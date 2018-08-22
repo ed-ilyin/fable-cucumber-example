521429d848fdb537ccaf358ffbf68a9dbf9eefc8
Ed Ilyin <ed.il@live.com>
2018-08-22 13:36:48 +0300

    initial commit

---
+++ tests/Tests.fs
@@ -0,0 +1,3 @@
+module Tests
+
+let x = 1
---
+++ tools/webpack.tests.config.js
@@ -0,0 +1,40 @@
+var CONFIG = {
+    fsharpEntry: "./tests/Tests.fsproj",
+    outputDir: "./test",
+    babel: {
+        presets: [
+            ["env", {
+                "modules": false,
+                "useBuiltIns": true,
+                "targets": { "node": "current" }
+            }]
+        ],
+    }
+}
+var path = require("path");
+module.exports = {
+    entry: { tests: ["core-js", CONFIG.fsharpEntry] },
+    output: {
+        path: path.join(__dirname, CONFIG.outputDir),
+        filename: '[name].js'
+    },
+    mode: "development",
+    devtool: "eval-source-map",
+    resolve: { symlinks: false },
+    module: {
+        rules: [
+            {
+                test: /\.fs(x|proj)?$/,
+                use: "fable-loader"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: CONFIG.babel
+                },
+            }
+        ]
+    }
+};
